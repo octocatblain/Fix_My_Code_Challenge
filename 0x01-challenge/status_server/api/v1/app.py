8dd9f8551830f47b4a1291b03b7c9b4462c14a6c
octocatblain <blainmasaku2@gmail.com>
2023-12-18 16:10:17 +0300

    fix: debug one

--- 0x01-challenge/status_server/api/v1/app.py
+++ 0x01-challenge/status_server/api/v1/app.py
@@ -12,8 +12,8 @@ app.register_blueprint(app_views)
 @app.errorhandler(404)
 def not_found(error):
     """ json 404 page """
-    return make_response(jsonify({"error": "Not found"}), 404)
-
+    # return make_response(jsonify({"error": "Not found"}), 404)
+    return jsonify({"error": "Not found"}), 404
 
 if __name__ == "__main__":
     # python -m api.v1.app 
